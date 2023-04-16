bee16f5e659637797733554613c7110367af06d9
SergeyOleks <oleksienkosa@gmail.com>
2023-04-17 00:15:11 +0300

    “ex1”

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,20 @@
- 
-console.log("")
+ const btnStart = document.querySelector("button[data-start]");
+ const btnStop = document.querySelector("button[data-stop]");
+ const body = document.querySelector('body');
+
+ btnStart.addEventListener('click',()=>{
+    timerId = setInterval(()=>{
+        btnStart.disabled = true;
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000
+    )
+ });
+
+ btnStop.addEventListener('click', ()=>{
+    clearInterval(timerId);
+    btnStart.disabled = false;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
