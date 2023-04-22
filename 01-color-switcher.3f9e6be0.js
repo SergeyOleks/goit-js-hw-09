f7affc690828403b8c2dc527ebd2a7268e83feb6
SergeyOleks <40609908+SergeyOleks@users.noreply.github.com>
2023-04-22 09:51:23 +0000

    Deploying to gh-pages from @ SergeyOleks/goit-js-hw-09@19cfe5b3fd364ecc2c0008f3a2af2abc287eff46 🚀

--- 01-color-switcher.3f9e6be0.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let o;t.addEventListener("click",(()=>{t.disabled=!0,o=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(o),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.3f9e6be0.js.map
